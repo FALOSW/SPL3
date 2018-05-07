c4f1a1f3fae6ad910f03a9aa1554ab6f0cdd6d96
FALOSW <pauli.proksch1109@gmail.com>
2018-05-07 14:33:58 +0200

    FizzBuzz, Version 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,19 +2,21 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
-			FizzerBuzzer(i);
+//		for (int i = 1; i <= 100; i++) {
+//			FizzerBuzzer(i);
+//		}
+		for (int i = 0; i <= 100; i+=4) {
+			FizzerBuzzer(i, false);
 		}
-
 	}
 
-	public static void FizzerBuzzer(int i) {
-		if (i % 7 == 0 && i % 4 == 0) {
+	public static void FizzerBuzzer(int i, boolean showFizzBuzz) {
+		if (i % 4 == 0 && i % 7 == 0 && showFizzBuzz == true) {
 			System.out.println("FizzerBuzzer");
 		} else if (i % 4 == 0) {
-			System.out.println("Fizzer");
+			System.out.println("Fizzer ("+ i +")");
 		} else if (i % 7 == 0) {
-			System.out.println("Buzzer");
+			System.out.println("Buzzer("+ i +")");
 		} else {
 			System.out.println(i);
 		}
